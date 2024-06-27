3dddc28ddb93b2534490bd715a01d9d27a8dc696
RavshanSean <ravshansean1995@gmail.com>
2024-06-27 10:03:47 -0400

    new

--- controllers/applications.js
+++ controllers/applications.js
@@ -13,7 +13,21 @@ router.get('/', (req, res) => {
 });
 
 router.get('/new', async (req, res) => {
-   res.render('applications/new.ejs');// new route
+   res.render('applications/new.ejs');// new route added
  });
+
+ router.post('/', async (req, res) => {
+   try {
+      const currentUser = await User.findById(req.session.user._id);
+
+      currentUser.applications.push(req.body);
+
+      await currentUser.save();
+      res.redirect(`/users/${currentUser._id}/applications`);
+    } catch (error) {
+      console.log(error);
+      res.redirect('/')
+    }
+  });
 
 module.exports = router;
